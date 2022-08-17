3ecad79a75fde393d561d1954603245abdf27836
AlrxDaley <alexander_daley@icloud.com>
2022-08-17 10:44:06 +0100

    added media and static files

--- pizzeria_app/env.py
+++ pizzeria_app/env.py
@@ -1,4 +1,4 @@
 import os
-os.environ["DATABASE_URL"] = 'postgres://gukewlciotylnh:523850f45071e3c2be610037f0e12ced7de71c593daf4b3e4bed0b7814d165d2@ec2-52-51-155-48.eu-west-1.compute.amazonaws.com:5432/d3je5hgvrhhvvh'
+os.environ["DATABASE_URL"] = 'postgres://mhochylcacaysn:44fae290a6548d79e089a30ba2ed0ffc4d69e036acc7f9ea0ba86550a718fc77@ec2-54-77-40-202.eu-west-1.compute.amazonaws.com:5432/ddk3gfrnoupl7f'
 os.environ["SECRET_KEY"] = 'Secret_key2000'
 os.environ["CLOUDINARY_URL"] = 'cloudinary://859435968898514:wKr-IzomNbfUz1BpGPSlu-Yhnk8@dkpchnxiu'
